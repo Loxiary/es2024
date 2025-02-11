dbd43eb6cb38f98a201143cd09a4b12a91ebee27
Tristan Leymonie <leymonie.t@gmail.com>
2025-02-11 13:27:15 +0100

    addedURLParam

--- public/template/js/manager.js
+++ public/template/js/manager.js
@@ -1,47 +1,47 @@
-const homeDiv = document.getElementById("home-div")
-const introductionDiv = document.getElementById("introduction-div")
-const div0101 = document.getElementById("0101-div")
-const div0102 = document.getElementById("0102-div")
-const div0103 = document.getElementById("0103-div")
-const div0201 = document.getElementById("0201-div")
-const div0202 = document.getElementById("0202-div")
-const div0203 = document.getElementById("0203-div")
-const div0301 = document.getElementById("0301-div")
-const div0302 = document.getElementById("0302-div")
-const div0303 = document.getElementById("0303-div")
-const conclusionDiv = document.getElementById("conclusion-div")
 const usernameText = document.getElementById("username-text")
 
-const texts = [homeDiv, introductionDiv, div0101, div0102, div0103, div0201, div0202, div0203, div0301, div0302, div0303, conclusionDiv]
+window.addEventListener('load', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const section = urlParams.get('section') || '0000';
+    
+    showSection(section);
+  });
 
-function switchText(text){
-    console.log(texts[text])
-    texts.forEach(element => {
-        element.style.display = "none";
-        if(element == texts[text]){element.style.display = "block"; }
-    });
-    // texts[text].style.display = "block";
+function switchText(btn){
+    const section = btn.dataset.section;
+
+    history.pushState({ section }, '', `?section=${section}`)
+
+    showSection(section);
 }
 
-function updateUsername(){
-    console.log("start updating username")
-    fetch('/update-username', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        })
-        .then(response => response.json())
-        .then(data => {
-        if (data.success) {
-            usernameText.textContent = "Menu";
-            console.log("username update done")
-        } else {
-            console.log("username update failed")
-            console.error(data.message);
-        }
-    }).catch(error => {
-        console.error('Erreur:', error)
-        console.log("username update error catched")
-    });
+function showSection(section){
+    document.querySelectorAll('.section').forEach(div => {
+        div.style.display = 'none';
+      });
+
+    const activeSection = document.getElementById(`section-${section}`);
+    if (activeSection) activeSection.style.display = 'block';
 }
+
+// function updateUsername(){
+//     fetch('/update-username', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//         if (data.success) {
+//             usernameText.textContent = "Menu";
+//             console.log("username update done")
+//         } else {
+//             console.log("username update failed")
+//             console.error(data.message);
+//         }
+//     }).catch(error => {
+//         console.error('Erreur:', error)
+//         console.log("username update error catched")
+//     });
+// }
